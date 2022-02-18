d9e1ed56dcb014b78a2d0440e12c5f2c32964110
Sasuke-7 <davidkunschventurini@gmail.com>
2022-02-18 02:33:53 -0300

    feat(Commands): add mvp command

---
+++ src/commands/match/mvp.ts
@@ -0,0 +1,100 @@
+import type { CommandInteraction } from 'discord.js';
+import type { TFunction } from 'i18next';
+
+import type { Yune } from '@client';
+import { Command } from '@structures/Command';
+import { MatchStatus } from '@utils/Constants';
+
+export default class extends Command {
+	constructor(client: Yune) {
+		super(client, {
+			name: 'mvp',
+			description: 'Define o jogador MVP da partida',
+			usage: '[usuário]',
+			showInMatchHelp: true,
+			options: [
+				{
+					name: 'usuário',
+					description: '@usuário que foi o jogador MVP da partida',
+					type: 'USER',
+				},
+			],
+		});
+	}
+
+	async run(interaction: CommandInteraction, t: TFunction) {
+		await interaction.deferReply();
+
+		const matchData = await interaction.client.database.matches.findOne(
+			{
+				status: MatchStatus.IN_GAME,
+				'channels.chat': interaction.channelId,
+			},
+			'participants'
+		);
+
+		if (!matchData?._id) {
+			interaction.editReply({
+				content: t('mvp.errors.invalid_match_channel'),
+			});
+			return;
+		}
+
+		if (
+			!matchData.participants.some((x) => x.userId === interaction.user.id) &&
+			!interaction.memberPermissions.has('ADMINISTRATOR')
+		) {
+			interaction.editReply({
+				content: t('mvp.errors.no_permission'),
+			});
+			return;
+		}
+
+		const target = interaction.options.getUser('usuário');
+		const currentMvp = matchData.participants.find((x) => x.mvp);
+
+		if (!target) {
+			if (currentMvp) {
+				interaction.editReply({
+					content: t('mvp.current_mvp', {
+						user: `<@!${currentMvp.userId}>`,
+					}),
+				});
+			} else {
+				interaction.editReply({
+					content: t('mvp.no_mvp'),
+				});
+			}
+			return;
+		}
+
+		if (target.bot || !matchData.participants.some((x) => x.userId === target.id)) {
+			interaction.editReply({
+				content: t('mvp.errors.invalid_user'),
+			});
+			return;
+		}
+
+		if (currentMvp?.userId === target.id) {
+			interaction.editReply({
+				content: t('mvp.errors.same_mvp'),
+			});
+			return;
+		}
+
+		await interaction.client.database.matches.update(matchData._id, {
+			$set: {
+				participants: matchData.participants.map((x) => ({
+					...x,
+					mvp: x.userId === target.id,
+				})),
+			},
+		});
+
+		interaction.editReply({
+			content: t('mvp.changed', {
+				user: target.toString(),
+			}),
+		});
+	}
+}
