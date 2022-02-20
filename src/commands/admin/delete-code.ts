4e7f08b476fcff14197906658e1c8629fa6382af
Sasuke-7 <davidkunschventurini@gmail.com>
2022-02-19 22:40:12 -0300

    add delete-code command

---
+++ src/commands/admin/delete-code.ts
@@ -0,0 +1,44 @@
+import type { CommandInteraction } from 'discord.js';
+import type { TFunction } from 'i18next';
+
+import type { Yune } from '@client';
+import { Command } from '@structures/Command';
+
+export default class extends Command {
+	constructor(client: Yune) {
+		super(client, {
+			name: 'deletar-codigo',
+			description: 'Deleta um código de assinatura permanentemente',
+			usage: '<código>',
+			permissions: ['ADMINISTRATOR'],
+			options: [
+				{
+					name: 'codigo',
+					description: 'O código da assinatura que deseja deletar',
+					type: 'STRING',
+					required: true,
+				},
+			],
+		});
+	}
+
+	async run(interaction: CommandInteraction, t: TFunction) {
+		await interaction.deferReply({ ephemeral: true });
+
+		const code = interaction.options.getString('codigo');
+		const subscriptionCode = await interaction.client.database.subscriptionCodes.findOne({ code }, '_id');
+
+		if (!subscriptionCode?._id) {
+			interaction.editReply({
+				content: t('delete_code.errors.invalid_code'),
+			});
+			return;
+		}
+
+		await interaction.client.database.subscriptionCodes.deleteOne(subscriptionCode._id);
+
+		interaction.editReply({
+			content: t('delete_code.deleted', { code }),
+		});
+	}
+}
