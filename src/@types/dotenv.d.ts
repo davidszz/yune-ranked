4d2c61ce7621a7dbd62fc6d554ecc48d97eae3db
Sasuke-7 <davidkunschventurini@gmail.com>
2022-02-20 23:35:44 -0300

    fixes & performance

--- src/@types/dotenv.d.ts
+++ src/@types/dotenv.d.ts
@@ -1,9 +1,8 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			DEVELOPMENT_TOKEN: string;
-			DEVELOPMENT_GUILD_ID: string;
 			MONGODB_URI: string;
+			NODE_ENV?: string;
 		}
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
 import 'dotenv/config';
 
-import { Intents } from 'discord.js';
+import { Collection, Intents } from 'discord.js';
 
 import { Yune } from '@client';
 import { DBWrapper } from '@database/DBWrapper';
+import type { IClientSchema } from '@database/schemas/ClientSchema';
 import { Logger } from '@services/Logger';
 
+const clients: Collection<string, Yune> = new Collection();
+
 const database = new DBWrapper();
+
 database.connect().then(async () => {
+	if (process.env.NODE_ENV?.trim() === 'production') {
+		const changeStream = database.clients.model.watch(null, { fullDocument: 'updateLookup' });
+		changeStream.on('change', async (change) => {
+			const fullDocument = change.fullDocument as IClientSchema;
+
+			if (['insert', 'update'].includes(change.operationType)) {
+				if (checkNewBot(fullDocument)) {
+					const client = await createClient(fullDocument);
+					await client.start();
+				}
+			} else if (change.operationType === 'delete') {
+				const documentKey = (change.documentKey as unknown as { _id: string })._id;
+				if (documentKey) {
+					const clientId = clients.findKey((x) => x.user?.id === documentKey) || documentKey;
+					const client = clients.get(clientId);
+					if (client) {
+						client.destroy();
+						clients.delete(clientId);
+					}
+				}
+			}
+		});
+	}
+
 	const clientsData = await database.clients.findMany({
 		expiresAt: {
 			$gte: new Date(),
@@ -17,16 +45,51 @@ database.connect().then(async () => {
 	Logger.info(`Initializing ${clientsData.length} clients!`);
 
 	for (const clientData of clientsData) {
-		const client = new Yune({
-			token: clientData.token,
-			guildId: clientData.guildId,
-			intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
-			database,
-		});
-
+		const client = await createClient(clientData);
 		await client.start();
-		console.log('-'.repeat(24));
+
+		console.log('—'.repeat(36));
 	}
 });
+
+function checkNewBot(data: IClientSchema) {
+	if (!data) {
+		return false;
+	}
+
+	const clientId = clients.findKey(
+		(x) => x.user?.id === data._id || x.guildId === data.guildId || x.token === data.token
+	);
+	if (!clientId) {
+		return true;
+	}
+
+	const client = clients.get(clientId);
+	if (
+		client.token === data.token &&
+		client?.user.id === data._id &&
+		client.guildId === data.guildId &&
+		client.readyAt
+	) {
+		return false;
+	}
+
+	client.destroy();
+	clients.delete(clientId);
+
+	return true;
+}
+
+async function createClient(data: IClientSchema) {
+	const client = new Yune({
+		token: data.token,
+		guildId: data.guildId,
+		intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES],
+		database,
+	});
+
+	clients.set(data._id, client);
+	return client;
+}
 
 process.on('unhandledRejection', console.error);
